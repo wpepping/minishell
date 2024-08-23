6fa0c8498ba1e8359d608d73c509df1d6abb760d
Paul Hartman <paul.hartman222@gmail.com>
2024-08-24 01:26:17 +0200

    untested fully list implemented

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: phartman <phartman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 19:22:33 by wpepping          #+#    #+#             */
-/*   Updated: 2024/08/24 00:31:39 by phartman         ###   ########.fr       */
+/*   Updated: 2024/08/24 01:25:53 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,56 +40,49 @@ int	get_builtin_index(char *token)
 	return (-1);
 }
 
-int	get_args(int index, char **tokens, t_parse_node *node)
+int	get_args(t_list *tokens, t_parse_node *node)
 {
 	int	i;
 	int	argc;
+	t_list	*tmp;
+
+	tmp = tokens;
 
 	argc = 0;
 	i = 0;
-	while (tokens[index + argc] && strcmp(tokens[index + argc], "<") != 0
-		&& strcmp(tokens[index + argc], ">") != 0 && strcmp(tokens[index + argc],
-			"|") != 0)
+	while(tokens->next && tokens->content.type == WORD)
+	{
 		argc++;
+		tokens = tokens->next;
+	}
+	tokens = tmp;
 	node->argv = malloc(sizeof(char *) * (argc + 1));
 	while (i < argc)
 	{
-		node->argv[i] = strdup(tokens[index]);
+		node->argv[i] = strdup(tokens->content.value);
 		if (!node->argv[i])
 		{
 			printf("Error: malloc failed\n");
 			exit(1);
 		}
 		i++;
-		index++;
+		tokens->next;
 	}
 	node->argv[i] = NULL;
 	return (argc);
 }
 
-int	handle_redirects(char **tokens, int index, t_parse_node *node)
+int	handle_redirects(t_list *tokens, t_parse_node *node)
 {
-
-	int jump;
-
-	jump = 0;
-	if (ft_strncmp(tokens[index], ">>", 2) == 0)
+	if (tokens->content.type == APPEND)
+	{
+		ft_lstadd_back(&node->output_dest, ft_lstnew(ft_strdup(tokens->next->content.value)));
 		node->append = true;
-	if (ft_strncmp(tokens[index], ">", 1) == 0)
-	{
-		ft_lstadd_back(&node->output_dest, ft_lstnew(ft_strdup(tokens[index
-						+ 1])));
-		jump= 2;
 	}
-	//else if (ft_strncmp(tokens[index], "<<", 2) == 0)
-		//node->heredoc = true;
-	else if (ft_strncmp(tokens[index], "<", 1) == 0)
-	{
-		ft_lstadd_back(&node->input_src, ft_lstnew(ft_strdup(tokens[index
-						+ 1])));
-		jump = 2;
-	}
-	return (jump);
+	else if (tokens->content.type == REDIRECT_OUT)
+		ft_lstadd_back(&node->output_dest, ft_lstnew(ft_strdup(tokens->next->content.value)));
+	else if (tokens->content.type == REDIRECT_IN)
+		ft_lstadd_back(&node->input_src, ft_lstnew(ft_strdup(tokens->next->content.value)));
 }
 
 bool in_quotes(char * token)
@@ -213,8 +206,7 @@ void	parse(t_data *data, char *cmd)
 {
 	t_parse_node	*node;
 	t_list			*tokens;
-	t_list			**head;
-	int				i;
+	t_list			*head;
 	char			*quote_arg;
 	quote_arg = NULL;
 	i= 0;
@@ -224,47 +216,44 @@ void	parse(t_data *data, char *cmd)
 		data->exit = 1;
 		return ;
 	}
-	tokens = ft_split(cmd, ' ');
+	tokens = tokenize(cmd);
+	head = tokens;
+	//tokens = ft_split(cmd, ' ');
 	node = create_parse_node();
 	
 	while (tokens->next)
 	{
-		if((in_quotes(tokens[i]) == true))
-		{
-			if(quote_arg == NULL)
-				quote_arg = ft_strdup(tokens[i]);
-			else
-			{
-				char *temp = ft_strjoin(quote_arg, " ");
-				free(quote_arg);
-				quote_arg = ft_strjoin(temp, tokens[i]);
-				free(temp);
-			}
-			i++;
-			continue;
-		}
-		if(ft_strncmp(tokens[i], "|", 1) == 0)
+		
+		if(tokens->content.type == PIPE)
 		{
 			print_argv(node);
 			ft_lstadd_back(&data->node_list, ft_lstnew(node));
 			node = create_parse_node();
-			i++;
+			tokens = tokens->next;
 			continue;
 			
 		}
-		if (access(tokens[i], X_OK) == 0)
+		if (access(tokens->content.value, X_OK) == 0)
 		{
-			node->exec = ft_strdup(tokens[i]);
+			node->exec = ft_strdup(tokens->content.value);
 			i += get_args(i, tokens, node);
+			while(tokens->next && tokens->content.type == WORD && --i < 0)
+				tokens = tokens->next;
 		}
-		else if (get_builtin_index(tokens[i]) != -1)
+		else if (get_builtin_index(tokens->content.value) != -1)
 		{
 			node->is_builtin = true;
-			node->exec = ft_strdup(tokens[i]);
-			i += get_args(i, tokens, node);
+			node->exec = ft_strdup(tokens->content.value);
+			while(tokens->next && tokens->content.type == WORD && --i < 0)
+				tokens = tokens->next;
 		}
 		else
-			i += handle_redirects(tokens, i, node);
+		{
+			handle_redirects(tokens, i, node);
+			tokens = tokens->next;
+			tokens = tokens->next;
+		}
+			
 	}
 	node->is_last = true;
 	print_argv(node);
